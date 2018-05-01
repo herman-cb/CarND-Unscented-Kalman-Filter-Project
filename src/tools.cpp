dbeb9707d70e90d04ab97c2fe8415674a6f2e956
HS <herman.sahota@gmail.com>
2018-05-01 13:24:54 -0700

    Add rmse computation.

--- src/tools.cpp
+++ src/tools.cpp
@@ -17,5 +17,12 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   */
   VectorXd rmse = VectorXd(4);
   rmse.fill(0.0);
+  assert(estimations.size() == ground_truth.size());
+
+  for (int i = 0; i < estimations.size(); i++) {
+      VectorXd diff = estimations[i] - ground_truth[i];
+      rmse += diff.cwiseProduct(diff);
+  }
+  rmse /= estimations.size();
   return rmse;
 }
